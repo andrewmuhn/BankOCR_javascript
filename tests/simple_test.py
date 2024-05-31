f58d0f74af18034eb0ed507cf0c6f96c6735a451
Andrew, Muhn <Andrew.Muhn@cvent.com>
2024-05-31 10:26:12 -0700

    setup

--- tests/simple_test.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from src import parser
-
-
-class TestParser(unittest.TestCase):
-
-    def should_parse_zeros(self):
-        with open('test_data/zeros.txt') as file:
-            self.assertEqual(parser.parse(file.read()), '000000000')
